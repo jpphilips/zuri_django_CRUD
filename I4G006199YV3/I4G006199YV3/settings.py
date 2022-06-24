4b5d9cbaf4707e77dfa1fb6901984d727fb3cc54
jpphilips <78326013+jpphilips@users.noreply.github.com>
2022-06-24 15:23:22 +0100

    updated templates and models

--- I4G006199YV3/I4G006199YV3/settings.py
+++ I4G006199YV3/I4G006199YV3/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'I4G006199YV3.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'blog/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
